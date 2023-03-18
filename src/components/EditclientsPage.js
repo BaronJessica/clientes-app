edfa683d5db37b0e11895bc8b850b368af7dbcf0
BaronJessica <jessicabaron93@gmail.com>
2023-03-18 20:34:03 -0300

    ...

--- src/components/EditclientsPage.js
+++ src/components/EditclientsPage.js
@@ -11,26 +11,38 @@ const EditclientsPage = () => {
     email:'',
     celular:''
   });
+
+  function handleChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    
+    var uptatedClient = {...cliente}
+    uptatedClient[name]= value;
+
+    setCliente(uptatedClient)
+  }
+
   return (
     <>
-      <MenuBar/> <div>
+      <MenuBar/> <div>0
       <h1>Editar Cliente Page </h1>
       <Form>
         <FormGroup>
           <Label for="cpf">CPF</Label>
-          <Input type='text' name='cpf' id='cpf'/>
+          <Input type='text' name='cpf' id='cpf' onChange={handleChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="nome">Nome</Label>
-          <Input type='text' name='nome' id='nome'/>
+          <Input type='text' name='nome' id='nome' onChange={handleChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="email">E-mail</Label>
-          <Input type='text' name='email' id='email'/>
+          <Input type='text' name='email' id='email' onChange={handleChange}/>
         </FormGroup>
         <FormGroup>
           <Label for="celular">Celular</Label>
-          <Input type='text' name='celular' id='celular'/>
+          <Input type='text' name='celular' id='celular' onChange={handleChange}/>
         </FormGroup>
 
         <FormGroup>
